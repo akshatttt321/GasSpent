4b07da8ac938beed7ae152ac7283b1f58f36803a
Akshat <akshatpersonal321@gmail.com>
2024-10-29 16:09:15 +0530

    added list of transactions

--- Total_Gas_Spent/src/App.jsx
+++ Total_Gas_Spent/src/App.jsx
@@ -6,6 +6,11 @@ import './App.css'
 
 function App() 
 {
+  const [className, setClassName] = useState('');
+  const [handleDiv,setHandleDiv] = useState(false)
+  const[timeStamp,setTimeStamp] = useState()  
+  const[TransactionsArray,setTransactionArray] = useState([])
+  const [showTransactions,setShowTransactions] = useState(false)
   const ETHapiKey = config.VITE_API_ETHKEY;
   const BaseapiKey = config.VITE_API_BASEKEY;
   const OPapiKey = config.VITE_API_OPKEY;
@@ -28,6 +33,7 @@ function App()
   useEffect(()=>{
   const controller = new AbortController()
   const signal = controller.signal
+  let transactionArray = []
     const fetchEthPrice = async()=>{
       const response = await fetch(`https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${ETHapiKey}`)
       const data = await response.json()
@@ -41,41 +47,45 @@ function App()
     try{
       let data
       if(chain==='Ethereum'){
-      const response = await fetch(`https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${trans[i]}&apikey=${ETHapiKey}`, {signal})
+      const response = await fetch(`https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${trans[i]}&apikey=${ETHapiKey}`, {signal})      
       if(!response.ok)
       throw new Error(`HTTP ERROR! ${response.status}`)
-      data = await response.json()
+      data = await response.json() 
+      transactionArray.push(data)
 }
 if(chain==='Base'){
   const response = await fetch(`https://api.basescan.org/api?module=proxy&action=eth_getTransactionByHash&txhash=${trans[i]}&apikey=${BaseapiKey}`, {signal})
   if(!response.ok)
   throw new Error(`HTTP ERROR! ${response.status}`)
   data = await response.json()
+  transactionArray.push(data)
 }
 if(chain==='Optimism'){
   const response = await fetch(`https://api-optimistic.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${trans[i]}&apikey=${OPapiKey}`,  {signal})
   if(!response.ok)
   throw new Error(`HTTP ERROR! ${response.status}`)
   data = await response.json()
+  transactionArray.push(data)
 }
 if(chain==='Arbitrum'){
   const response = await fetch(`https://api.arbiscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${trans[i]}&apikey=${ArbapiKey}`,  {signal})
   if(!response.ok)
   throw new Error(`HTTP ERROR! ${response.status}`)
   data = await response.json()
+  transactionArray.push(data)
 }
 if(chain==='Linea'){
   const response = await fetch(`https://api.lineascan.build/api?module=proxy&action=eth_getTransactionByHash&txhash=${trans[i]}&apikey=${LineaapiKey}`,  {signal})
   if(!response.ok)
   throw new Error(`HTTP ERROR! ${response.status}`)
   data = await response.json()
+  transactionArray.push(data)
 }
        let gas = parseInt(data.result.gas)
        let gasprice = parseInt(data.result.gasPrice)
        let totalCost = gas*gasprice
        let totalCostEther = totalCost/1e18;
        totalgas = totalgas+totalCostEther;
-       console.log(totalgas)
    }
       catch (error){
         if(error.name === 'AbortError'){
@@ -88,6 +98,7 @@ if(chain==='Linea'){
     }
     if(i===trans.length)
     setGas(totalgas)
+    setTransactionArray(transactionArray)
   }
   fetchEthPrice()
   getGas()
@@ -97,24 +108,20 @@ if(chain==='Linea'){
   },[transaction])
 
   const handleSubmit=async()=> {
-    console.log('ETH API Key:', config.VITE_API_ETHKEY);
-    console.log('Base API Key:', config.VITE_API_BASEKEY);
-    console.log('Arbitrum API Key:', config.VITE_API_ARBKEY);
-    console.log('Optimism API Key:', config.VITE_API_OPKEY);
-    console.log('Linea API Key:', config.VITE_API_LINEAKEY);
-       
+    setTransactionArray()
     setGas(0)
+    setTimeStamp()
+    setShowTransactions(false)
     if(address!=undefined)
     setSecondDiv(true)
   if(chain==='Ethereum'){
   try{
-   const response = await fetch(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10000&sort=asc&apikey=${ETHapiKey}`)
+   const response = await fetch(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10000&sort=desc&apikey=${ETHapiKey}`)
 
    if(!response.ok)
     throw new Error(`HTTP ERROR! ${response.status}`)
     const data = await response.json()
     setTransaction(data)
-    console.log(transaction)
 }
    catch (error){
     console.error("fetch error",error)
@@ -122,13 +129,12 @@ if(chain==='Linea'){
   }
   if(chain ==='Base'){
     try{
-      const response = await fetch(`https://api.basescan.org/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10000&sort=asc&apikey=${BaseapiKey}`)
+      const response = await fetch(`https://api.basescan.org/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10000&sort=desc&apikey=${BaseapiKey}`)
    
       if(!response.ok)
        throw new Error(`HTTP ERROR! ${response.status}`)
        const data = await response.json()
        setTransaction(data)
-       console.log(transaction)
    }
       catch (error){
        console.error("fetch error",error)
@@ -136,13 +142,12 @@ if(chain==='Linea'){
    }
    if(chain ==='Optimism'){
     try{
-      const response = await fetch(`https://api-optimistic.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10000&sort=asc&apikey=${OPapiKey}`)
+      const response = await fetch(`https://api-optimistic.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10000&sort=desc&apikey=${OPapiKey}`)
    
       if(!response.ok)
        throw new Error(`HTTP ERROR! ${response.status}`)
        const data = await response.json()
        setTransaction(data)
-       console.log(transaction)
    }
       catch (error){
        console.error("fetch error",error)
@@ -150,13 +155,12 @@ if(chain==='Linea'){
    }
    if(chain ==='Arbitrum'){
     try{
-      const response = await fetch(`https://api.arbiscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=latest&page=1&offset=10000&sort=asc&apikey=${ArbapiKey}`)
+      const response = await fetch(`https://api.arbiscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=latest&page=1&offset=10000&sort=desc&apikey=${ArbapiKey}`)
    
       if(!response.ok)
        throw new Error(`HTTP ERROR! ${response.status}`)
        const data = await response.json()
        setTransaction(data)
-       console.log(transaction)
    }
       catch (error){
        console.error("fetch error",error)
@@ -164,13 +168,14 @@ if(chain==='Linea'){
    }
    if(chain ==='Linea'){
     try{
-      const response = await fetch(`https://api.lineascan.build/api?module=account&action=txlist&address=${address}&startblock=0&endblock=latest&page=1&offset=10000&sort=asc&apikey=${LineaapiKey}`)
+      const response = await fetch(`https://api.lineascan.build/api?module=account&action=txlist&address=${address}&startblock=0&endblock=latest&page=1&offset=10000&sort=desc&apikey=${LineaapiKey}`)
    
       if(!response.ok)
        throw new Error(`HTTP ERROR! ${response.status}`)
        const data = await response.json()
        setTransaction(data)
-       console.log(transaction)
+
+       
    }
       catch (error){
        console.error("fetch error",error)
@@ -178,10 +183,55 @@ if(chain==='Linea'){
    }
    
 }
+const handleShowTransactions = async()=>{
+  setShowTransactions(!showTransactions)
+  if(!timeStamp){
+  const fetchTimeStamp = async()=>{
+    let timestamp = []
+    let response
+    for(let i=0;i<TransactionsArray.length;i++){
+    if(chain==='Ethereum')
+    response = await fetch(`https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=${TransactionsArray[i].result.blockNumber}&boolean=true&apikey=${ETHapiKey}`)
+    if(chain==='Optimism')
+    response = await fetch(`https://api-optimistic.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=${TransactionsArray[i].result.blockNumber}&boolean=true&apikey=${OPapiKey}`)
+    if(chain==='Linea')
+    response = await fetch(`https://api.lineascan.build/api?module=proxy&action=eth_getBlockByNumber&tag=${TransactionsArray[i].result.blockNumber}&boolean=true&apikey=${LineaapiKey}`)
+    if(chain==='Arbitrum')
+    response = await fetch(`https://api.arbiscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=${TransactionsArray[i].result.blockNumber}&boolean=true&apikey=${ArbapiKey}`)
+    if(chain==='Base')
+    response = await fetch(`https://api.basescan.org/api?module=proxy&action=eth_getBlockByNumber&tag=${TransactionsArray[i].result.blockNumber}&boolean=true&apikey=${BaseapiKey}`) 
+    if(!response.ok)
+    throw new Error(`HTTP ERROR! ${response.status}`)
+    const data2 = await response.json()
+    timestamp.push(data2.result.timestamp)
+  }
+  setTimeStamp(timestamp)
+  }
+    
+  fetchTimeStamp()
+  console.log(timeStamp)
+}
+}
+
+useEffect(() => {
+  if (!showTransactions) {
+    const timer = setTimeout(() => {
+      setClassName('overflow-hidden'); // Set the desired class after 2 seconds
+    }, 2000);
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount or when showTransactions changes
+  } else {
+    setClassName(''); // Reset the class when showTransactions is true
+  }
+}, [showTransactions]);
 
     return (
-    <div className={`bg-gray-900 flex h-screen w-screen justify-center items-center `}>
-        <div className={`bg-zinc-800 h-1/2 flex flex-col w-1/2 justify-center items-center rounded-lg ${secondDiv?"move-aside":''} ${secondDiv?"ml-20":''} transition-all`
+      <div className={`${showTransactions?"move-up":'move-down'} h-screen ${className}`} 
+      >
+    <div className={`bg-gray-900 flex flex-col h-screen w-screen justify-center items-center`}>
+        <h1 className='text-white font-semibold font-mono text-2xl mt-10 underline underline-offset-[16px] decoration-dashed decoration-gray-500 border-x-2 border-t-2 border-gray-500 p-2'>Calculate Your Total Gas Expenditure</h1>
+      <div className={`bg-gray-900 flex h-full w-full justify-center items-center `}>
+        <div className={`bg-zinc-800 h-[50vh] flex flex-col w-[50vw] justify-center items-center rounded-lg  ${secondDiv?"move-aside":''} ${secondDiv?"ml-20":''} transition-all`
       } style={{}}>
         <div className='flex justify-center items-center'>
         <div className='flex flex-col'>
@@ -222,8 +272,52 @@ if(chain==='Linea'){
           <h1 className='mt-2 flex items-center'>Total gas spent in USD:  {Gas!=0?`${(Gas*EthPrice).toPrecision(7)} USD`:(<img className='ml-[14px] aspect-video h-8' src='https://i.gifer.com/ZWdx.gif' alt='Calculating...'></img>)}</h1>
         </div>
         )
-
+      }
+        </div>
+        {Gas!=0 && <button className={`${showTransactions?'w-40 h-10 text-2xl':''} button1 text-white text-xl font-serif hover:underline hover:underline-offset-4 hover:text-blue-600`}
+        style={{transition:'fadeIN 2s'}}
+        onClick={handleShowTransactions}
+        >{showTransactions?"Click here to hide transactions":"Click here to show transactions"}</button>
         }
+    </div>
+    { transaction && TransactionsArray && (<div className={` ${showTransactions?'goOpaque':'goTransparent'} flex border-y-2 justify-center items-center text-white`}>
+      <div className='flex flex-col pb-2  border-x-2 px-6 justify-center items-center mr-4'>
+      <h1 className='my-2 font-mono font-semibold w-30 ml-4'>Transaction</h1>
+      {transaction.result.map((hash,index)=><p key={index} className='my-2'>{index+1}</p>)}
+    </div>
+      <div className='flex flex-col pb-2   border-r-2 px-2 justify-center items-center mr-4'>
+      <h1 className='my-2 font-mono font-semibold '>Transaction Hash</h1>
+      {transaction.result.map((hash,index)=><p key={index} className='my-2 w-60 overflow-hidden text-ellipsis whitespace-nowrap'>{hash.hash}</p>)}
+    </div>
+    <div className='flex flex-col pb-2   border-r-2 px-2 justify-center items-center mr-4'>
+      <h1 className='my-2 font-mono font-semibold'>From</h1>
+      {transaction.result.map((hash,index)=><p key={index} className='my-2 w-40 overflow-hidden text-ellipsis whitespace-nowrap'>{TransactionsArray[index].result.from}</p>)}
+    </div>
+    <div className='flex flex-col pb-2  border-r-2 px-2 justify-center items-center mr-4'>
+      <h1 className='my-2 font-mono font-semibold'>To</h1>
+      {transaction.result.map((hash,index)=><p key={index} className={TransactionsArray[index].result.to?'my-2 w-40 overflow-hidden text-ellipsis whitespace-nowrap':'my-2 w-40 overflow-hidden text-center text-ellipsis whitespace-nowrap'}>{TransactionsArray[index].result.to?TransactionsArray[index].result.to:'-'}</p>)}
+    </div>
+    <div className='flex flex-col pb-2  border-r-2 px-2 justify-center items-center mr-4'>
+      <h1 className='my-2 font-mono font-semibold whitespace-nowrap text-center w-36 -ml-4'>Trx Value(USD)</h1>
+      {transaction.result.map((hash,index)=><p key={index} className={((parseInt(TransactionsArray[index].result.value)/1e18)*EthPrice).toFixed(5)==0?'text-center my-2 w-20 overflow-hidden text-ellipsis -ml-2 whitespace-nowrap':'text-center -ml-2 my-2 w-20 overflow-hidden text-ellipsis whitespace-nowrap'}>{((parseInt(TransactionsArray[index].result.value)/1e18)*EthPrice).toFixed(10)==0?"Null":
+        ((parseInt(TransactionsArray[index].result.value)/1e18)*EthPrice)<0?
+        ((parseInt(TransactionsArray[index].result.value)/1e18)*EthPrice).toPrecision(5):
+        ((parseInt(TransactionsArray[index].result.value)/1e18)*EthPrice).toPrecision(8)}</p>)}
+    </div>
+
+    <div className='flex flex-col pb-2  border-r-2 px-2 justify-center items-center mr-4'>
+      <h1 className='my-2 font-mono font-semibold w-40'>Gas used in USD</h1>
+      {transaction.result.map((hash,index)=><p key={index} className='my-2 w-20 overflow-hidden text-ellipsis whitespace-nowrap'>{((parseInt((TransactionsArray[index].result.gas)*parseInt(TransactionsArray[index].result.gasPrice))/1e18)*EthPrice).toFixed(5)}</p>)}
+    </div>
+    <div className='flex flex-col pb-2  border-r-2 px-2 justify-center items-center mr-4'>
+      <h1 className='my-2 font-mono font-semibold w-40'>Gas used in ETH</h1>
+      {transaction.result.map((hash,index)=><p key={index} className='my-2 w-20 text-ellipsis whitespace-nowrap'>{(parseInt((TransactionsArray[index].result.gas)*parseInt(TransactionsArray[index].result.gasPrice))/1e18).toFixed(5)<0.00001?"<0.00001":(parseInt((TransactionsArray[index].result.gas)*parseInt(TransactionsArray[index].result.gasPrice))/1e18).toFixed(5)}</p>)}
+    </div>
+    <div className='flex flex-col pb-2 border-r-2 px-2 justify-center items-center mr-4'>
+      <h1 className='my-2 font-mono font-semibold mr-2'>TimeStamp</h1>
+      {transaction.result.map((hash,index)=><p key={index} className='my-2 w-40 overflow-hidden text-ellipsis whitespace-nowrap mr-4'>{timeStamp?(new Date(parseInt(timeStamp[index],16)*1000)).toLocaleString():"Fetching..."}</p>)}
+    </div>
+    </div>)}
     </div>
   )
 }
